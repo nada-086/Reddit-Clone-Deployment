36e72fae65220ff3c32de4f0a9e62594105f9595
Nada Essa <nadaessa086@gmail.com>
2025-05-05 16:45:28 +0300

    Adding the ability to download the private key

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,6 +8,12 @@ resource "aws_key_pair" "ssh_public_key" {
     public_key = tls_private_key.ssh_private_key.public_key_openssh
 }
 
+resource "local_file" "ssh_key" {
+    filename = "private_key.pem"
+    content = tls_private_key.ssh_private_key.private_key_pem
+    file_permission = "0400"
+}
+
 
 resource "aws_security_group" "jenkins_sg" {
     name        = "jenkins_sg"
